acec25e651df0128031fb8fb1ea490a65b62c759
rsr83 <rafael.ramalho@gmail.com>
2024-01-15 16:29:34 +0000

    this commit add GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,7 +45,50 @@ describe("/api", () => {
         expect(Object.keys(response.body.api)).toEqual([
           "GET /api",
           "GET /api/topics",
+          "GET /api/articles",
         ]);
+      });
+  });
+});
+
+describe("/api/articles/:articles_id", () => {
+  test("GET 200: receive a the article requested by their respective id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.article.article_id).toBe(1);
+        expect(response.body.article.title).toBe(
+          "Living in the shadow of a great man"
+        );
+        expect(response.body.article.topic).toBe("mitch");
+        expect(response.body.article.author).toBe("butter_bridge");
+        expect(response.body.article.body).toBe(
+          "I find this existence challenging"
+        );
+        expect(response.body.article.created_at).toBe(
+          "2020-07-09T20:11:00.000Z"
+        );
+        expect(response.body.article.votes).toBe(100);
+        expect(response.body.article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
+  });
+  test("GET:404 sends an appropriate status and error message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Article not found");
+      });
+  });
+  test("GET:400 sends an appropriate status and error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-article")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad Request");
       });
   });
 });
